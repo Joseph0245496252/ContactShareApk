43cdd63fe5b87b5f3a98404d59dcba162396299e
Joseph Asante <freecoding1@gmail.com>
2020-05-17 23:17:31 +0000

    Project redered

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import LoginScreen from './src/screen/LoginScreen';
+import RegisterScreen from './src/screen/RegisterScreen';
+import GetInTouchScreen from './src/screen/GetInTouchScreen';
+import WelcomeScreen from './src/screen/WelcomeScreen';
 
+
+const Stack = createStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer> 
+      <Stack.Navigator initialRouteName="Welcome" >
+        <Stack.Screen name="Login" component={LoginScreen} 
+            options={{
+              headerTitle:"Sign In",
+              headerTitleAlign:"center",
+              headerStyle:{backgroundColor:"red"},
+              headerTintColor:"white"
+            }}
+        />
+        <Stack.Screen name="Register" component={RegisterScreen} 
+              options={{
+                headerTitle:"Register",
+                headerTitleAlign:"center",
+                headerStyle:{backgroundColor:"red"},
+                headerTintColor:"white"
+              }}
+        />
+        <Stack.Screen name="GetInTouch" component={GetInTouchScreen}
+            options={{
+              headerTitle:null,
+              headerStyle:{height:60}
+            }}
+        />
+        <Stack.Screen name="Welcome" component={WelcomeScreen}
+             options={{
+              headerTitle:null,
+              headerStyle:{height:0}
+            }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
